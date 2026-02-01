42fa3f472b5450cab6a798bbcc3a9c8740818c94
Ioan Chist <ioanchist@gmail.com>
2026-02-01 18:24:34 +0000

    File changes

--- src/components/transactions/QuickAddTransaction.jsx
+++ src/components/transactions/QuickAddTransaction.jsx
@@ -317,7 +317,7 @@ export default function QuickAddTransaction({ transaction, onClose, accounts, de
                 ref={cameraInputRef}
                 type="file"
                 accept="image/*"
-                capture="environment"
+                capture
                 onChange={(e) => {
                   if (e.target.files?.[0]) {
                     handleReceiptScan(e.target.files[0]);
